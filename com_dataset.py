24d64ea323445226dcca507465561b7b8ea4a9ad
LLPB-pixel <perezllorenc@gmail.com>
2026-07-22 23:27:40 +0200

    organizacion del dataset

--- com_dataset.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-for split in ["train", "val", "test"]:
-    df = pd.read_csv(f"Data/{split}.csv")
-    print(f"\n{split}:")
-    print(df["label"].value_counts(dropna=False))
-    print("Nulls:", df["label"].isna().sum())
